35ac56a9a10da8cdba18f44b231b663048f47ebd
Guilherme Di Paula <guilepaul@gmail.com>
2020-11-21 00:53:29 -0300

    navigation config

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const SignIn: React.FC = () => {
+    return (
+        <S.Container />
+    )
+}
+
+export default SignIn
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const SignUp: React.FC = () => {
+    return (
+        <S.Container />
+    )
+}
+
+export default SignUp
