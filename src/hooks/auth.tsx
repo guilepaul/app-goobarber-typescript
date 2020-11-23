9c597e1f1453c91b027fb1092488f6a767db60e3
Guilherme Di Paula <guilepaul@gmail.com>
2020-11-22 21:30:57 -0300

    loading

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -13,6 +13,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -21,6 +22,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
       async function loadStorageData(): Promise<void> {
@@ -33,6 +35,8 @@ const AuthProvider: React.FC = ({ children }) => {
                 setData({token: token[1], user: JSON.parse(user[1])})
                 
             }
+
+            setLoading(false)
 
       }
 
@@ -61,7 +65,7 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,24 @@
 import AsyncStorage from '@react-native-community/async-storage'
 import React from 'react'
+import {ActivityIndicator, View} from 'react-native'
+
 import AuthRoutes from './auth.routes'
+import AppRoutes from './app.routes'
+
+import {useAuth} from '../hooks/auth'
 
 const Routes: React.FC = () => {
-    return <AuthRoutes />
+    const {user, loading} = useAuth()
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center'}}>
+                <ActivityIndicator size='large' color= '666' />
+            </View>
+        )
+    }
+
+    return user ? <AppRoutes /> : <AuthRoutes />
 }
 
 export default Routes
